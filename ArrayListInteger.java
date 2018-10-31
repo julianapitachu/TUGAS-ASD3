9dcf615cf0204cb2088b89182346598a345936dc
julianapitachu <43759141+julianapitachu@users.noreply.github.com>
2018-10-31 10:48:01 -0700

    Create ArrayListInteger.java

---
+++ ArrayListInteger.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ArrayListInteger {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> nomorUrut = new ArrayList<Integer>();
+		
+		nomorUrut.add(1);
+		nomorUrut.add(2);
+		nomorUrut.add(3);
+		nomorUrut.add(4);
+		
+		System.out.println("Nomor Urut Siswa : " + nomorUrut.toString());
+		
+		
+
+	}
+
+}
