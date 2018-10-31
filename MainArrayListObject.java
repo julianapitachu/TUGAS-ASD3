3ef5c2040329cb7de3e4e6d70d316aef1a61af37
julianapitachu <43759141+julianapitachu@users.noreply.github.com>
2018-10-31 11:40:35 -0700

    Create MainArrayListObject.java

---
+++ MainArrayListObject.java
@@ -0,0 +1,13 @@
+
+public class MainArrayListObject {
+
+	public static void main(String[] args) {
+		Mahasiswa mhs1 = new Mahasiswa();
+		
+		mhs1.Nama = "Juliana";
+		mhs1.Nim = "D0217010";
+		mhs1.Kelas = "Teknik Informatika. A";
+		mhs1.Alamat = "Matakali";
+	}
+
+}
