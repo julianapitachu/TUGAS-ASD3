643f03a8a12445d9e57b8cd7d8e526a3c285babe
julianapitachu <43759141+julianapitachu@users.noreply.github.com>
2018-10-31 10:43:19 -0700

    Create ArrayListFloat.java

---
+++ ArrayListFloat.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ArrayListFloat {
+
+	public static void main(String[] args) {
+		ArrayList<Float> tinggiBadan = new ArrayList<Float>();
+		
+		tinggiBadan.add((float)145);
+		tinggiBadan.add((float)150);
+		tinggiBadan.add((float)156);
+		tinggiBadan.add((float)166);
+	
+		System.out.println("Tinggi Badan Mahasiswa : " + tinggiBadan.toString());
+
+	}
+
+}
