c5b8a9d93226469e0a8a6b09c78112c93fd448ef
julianapitachu <43759141+julianapitachu@users.noreply.github.com>
2018-10-31 10:46:45 -0700

    Create ArrayListDouble.java

---
+++ ArrayListDouble.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayListDouble {
+
+	public static void main(String[] args) {
+		ArrayList<Double> beratBadan= new ArrayList<Double>();
+		
+		beratBadan.add((double)40);
+		beratBadan.add((double)50);
+		beratBadan.add((double)55);
+		beratBadan.add((double)60);
+		
+		System.out.println("Daftar Berat Badan : " + beratBadan.toString());
+		
+
+	}
+
+}
